08eb310ef70924e02de03446e6a055a29372210d
Niall Ryan <niallcryan@live.ie>
2025-09-02 14:06:42 +0100

    Improved API, added documentation to parray header

--- include/collections/parray.h
+++ include/collections/parray.h
@@ -7,14 +7,90 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+/**
+ * @brief Pointer array
+ *
+ * A heterogenous dynamic data structure which handles its own memory.
+ */
 typedef struct parray_t c_parray_t;
 
-c_parray_t *array_create();
-void array_free(c_parray_t *array);
-const void *array_append(c_parray_t *array, const void *item, const size_t size);
-const void *array_get(c_parray_t *array, size_t index);
-size_t array_length(c_parray_t *array);
-const void *array_insert(c_parray_t *array, size_t index, const void *item, size_t size);
-void *array_pop(c_parray_t *array, size_t index);
+/**
+ *
+ * @brief Creates a new pointer array.
+ *
+ * A dynamic pointer array is created, allocating memory for
+ * its internals.
+ * The pointer array automatically grows its size as required.
+ *
+ * @return Pointer to pointer array, or NULL on failure.
+ *
+ * @note Must free via `parray_free`.
+ */
+c_parray_t *parray_create();
+
+
+/**
+ *
+ * @brief Frees a pointer array.
+ *
+ * Frees all memory associated with the array.
+ *
+ * @param parray The pointer array to free.
+ */
+void parray_free(c_parray_t *parray);
+
+/**
+ *
+ * @brief Appends an `item` of `size` to `parry`.
+ *
+ * Appends an `item` of `size` to `parray`.
+ * Copies the value of `item` and stores it.
+ * Grows the pointer array if needed.
+ *
+ * @param parray The pointer array to append to.
+ * @param item The item whos value to append.
+ * @param size The size of the item's value to append.
+ * @return NULL on error, or the pointer array's reference of the new value.
+ */
+const void *parray_append(c_parray_t *parray, const void *item, const size_t size);
+
+/**
+ *
+ * @brief Retrieves an item from the pointer array.
+ *
+ * Retrieves the pointer array's copied value at an index.
+ *
+ * @param parray The pointer array to get from.
+ * @param index The index from which to get.
+ * @return NULL on error, or the item at the index within the pointer array.
+ */
+const void *parray_get(c_parray_t *parray, size_t index);
+
+/**
+ *
+ * @brief Retrieves the length of a pointer array.
+ *
+ * Retrieves the length of a pointer array.
+ *
+ * @param parray The pointer array from which to retrieve the length of.
+ * @return The length of the pointer array.
+ */
+size_t parray_length(c_parray_t *array);
+
+/**
+ *
+ * @brief Inserts an item into the pointer array.
+ *
+ * Copies the value of `item` and inserts it into `parray`
+ * at `index`.
+ *
+ * @param parray The array to insert into.
+ * @param index The index to insert to.
+ * @param item The item to copy the value from and insert.
+ * @param size The size of the value to copy from `item`.
+ * @return NULL on error, or the pointer array's reference of the copied inserted value.
+ */
+const void *parray_insert(c_parray_t *array, size_t index, const void *item, size_t size);
+void *parray_pop(c_parray_t *array, size_t index);
 
 #endif
--- src/parray.c
+++ src/parray.c
@@ -18,7 +18,7 @@ struct parray_t {
   size_t allocation_size;
 };
 
-parray_t *array_create() {
+parray_t *parray_create() {
   parray_t *new_arr = (parray_t *)malloc(sizeof(parray_t));
   if (new_arr == NULL) {
     return NULL;
@@ -40,29 +40,29 @@ parray_t *array_create() {
   return new_arr;
 }
 
-void array_free(parray_t *array) {
-  for (size_t i = 0; i < array->length; i++) {
-    free(array->items[i]);
+void parray_free(parray_t *parray) {
+  for (size_t i = 0; i < parray->length; i++) {
+    free(parray->items[i]);
   }
-  free(array->items);
-  free(array);
+  free(parray->items);
+  free(parray);
 }
 
-bool __array_grow(parray_t *array) {
-  size_t new_capacity = CALCULATE_RESIZE(array->allocation_size);
-  void **new_items = realloc(array->items, sizeof(void*) * new_capacity);
+bool __parray_grow(parray_t *parray) {
+  size_t new_capacity = CALCULATE_RESIZE(parray->allocation_size);
+  void **new_items = realloc(parray->items, sizeof(void*) * new_capacity);
   if (new_items == NULL) {
     return false;
   }
-  array->items = new_items;
-  memset(&array->items[array->allocation_size], 0, sizeof(void*) * (new_capacity - array->allocation_size));
-  array->allocation_size = new_capacity;
+  parray->items = new_items;
+  memset(&parray->items[parray->allocation_size], 0, sizeof(void*) * (new_capacity - parray->allocation_size));
+  parray->allocation_size = new_capacity;
 
   return true;
 }
 
 // Return the item on succesful append, or NULL on error
-const void *array_append(parray_t *array, const void *item, const size_t size) {
+const void *parray_append(parray_t *parray, const void *item, const size_t size) {
   // Allocate memory first incase it fails
   void *new_item = malloc(size);
   if (new_item == NULL) {
@@ -70,40 +70,40 @@ const void *array_append(parray_t *array, const void *item, const size_t size) {
   }
 
   // Check if we need to resize
-  if (array->length == array->allocation_size) {
-    if (!__array_grow(array)) {
+  if (parray->length == parray->allocation_size) {
+    if (!__parray_grow(parray)) {
       free(new_item);
       return NULL;
     }
   }
 
   // Copy object by value
-  array->items[array->length] = new_item;
-  memcpy(array->items[array->length], item, size);
+  parray->items[parray->length] = new_item;
+  memcpy(parray->items[parray->length], item, size);
 
-  array->length++;
+  parray->length++;
 
-  return array->items[array->length - 1];
+  return parray->items[parray->length - 1];
 }
 
-const void *array_get(parray_t *array, size_t index) {
-  if (index + 1 > array->length || index < 0) {
+const void *parray_get(parray_t *parray, size_t index) {
+  if (index + 1 > parray->length || index < 0) {
     return NULL;
   }
-  return array->items[index];
+  return parray->items[index];
 }
 
-size_t array_length(parray_t *array) { return array->length; };
+size_t parray_length(parray_t *parray) { return parray->length; };
 
 // Only allows insertion from 0 < length
-const void *array_insert(parray_t *array, size_t index, const void *item, size_t size) {
+const void *parray_insert(parray_t *parray, size_t index, const void *item, size_t size) {
   // Check if the insertion is within bounds
-  if (index < 0 || array->length < index) {
+  if (index < 0 || parray->length < index) {
     return NULL;
   }
 
   // If it's inserted to the end, it's just an append
-  if (index == array->length) return array_append(array, item, size);
+  if (index == parray->length) return parray_append(parray, item, size);
 
   // Allocate memory for new item early incase it fails
   void *new_item = malloc(size);
@@ -112,71 +112,71 @@ const void *array_insert(parray_t *array, size_t index, const void *item, size_t
   }
 
   // Resize if we need to
-  if (array->length + 1 == array->allocation_size) {
-    if (!__array_grow(array)) {
+  if (parray->length + 1 == parray->allocation_size) {
+    if (!__parray_grow(parray)) {
       free(new_item);
       return NULL;
     }
   }
 
   // Move memory forward
-  memmove(&array->items[index + 1], &array->items[index], sizeof(void*) * (array->length - index));
+  memmove(&parray->items[index + 1], &parray->items[index], sizeof(void*) * (parray->length - index));
 
   // Copy value and place it in our array
-  array->items[index] = new_item;
-  memcpy(array->items[index], item, size);
+  parray->items[index] = new_item;
+  memcpy(parray->items[index], item, size);
 
-  array->length += 1;
+  parray->length += 1;
 
-  return array->items[index];
+  return parray->items[index];
 }
 
 // User has ownership over this now
-void *array_pop(parray_t *array, size_t index) {
+void *parray_pop(parray_t *parray, size_t index) {
   // Ensure we're within bounds
-  if (index < 0 || array->length < index) {
+  if (index < 0 || parray->length < index) {
     return NULL;
   }
 
   // Keep a reference to the item incase we overwrite the index
-  void *to_remove = array->items[index];
-  array->length--;
+  void *to_remove = parray->items[index];
+  parray->length--;
 
   // If it's the final item, we don't have to move any memory
-  if (index == array->length) {
+  if (index == parray->length) {
     return to_remove;
   }
 
   // Move the memory back
-  memmove(&array->items[index], &array->items[index + 1], sizeof(void*) * (array->length - index));
+  memmove(&parray->items[index], &parray->items[index + 1], sizeof(void*) * (parray->length - index));
 
   return to_remove;
 }
 
 int main(void) {
-  parray_t *myarr = array_create();
+  parray_t *myarr = parray_create();
   
   for (int i = 0; i < 100; i++) {
     printf("appending '%d'...\n", i);
-    const int *ret = (int*)array_append(myarr, &i, sizeof i);
+    const int *ret = (int*)parray_append(myarr, &i, sizeof i);
     if (ret == NULL) {
       printf("FAILED TO APPEND '%d'!\n", i);
     }
     printf("Yippee!!!\n");
   }
 
-  array_insert(myarr, 0, "MEOW", strlen("MEOW") + 1);
+  parray_insert(myarr, 0, "MEOW", strlen("MEOW") + 1);
 
-  char *msg = (char*)array_pop(myarr, 0);
+  char *msg = (char*)parray_pop(myarr, 0);
   printf("msg: %s\n", msg);
   free(msg);
 
   for (int i = 0; i < myarr->length; i++) {
-    if (i == -1) printf("arr[%d] = %s\n", i, (char*)array_get(myarr, i));
-    else printf("arr[%d] = %d\n", i, *(int*)array_get(myarr, i));
+    if (i == -1) printf("arr[%d] = %s\n", i, (char*)parray_get(myarr, i));
+    else printf("arr[%d] = %d\n", i, *(int*)parray_get(myarr, i));
   }
 
   printf("Freeing array\n");
-  array_free(myarr);  
+  parray_free(myarr);  
   return 0;
 }
